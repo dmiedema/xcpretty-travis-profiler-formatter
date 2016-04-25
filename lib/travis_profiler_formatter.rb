b117e26002fa1a1cb7fcb17d06c7853cef618a4f
dmiedema <danielmiedema+github@me.com>
2016-04-25 08:36:48 -0700

    Combine travis output and profiler

--- lib/travis_profiler_formatter.rb
+++ lib/travis_profiler_formatter.rb
@@ -1,4 +1,4 @@
-class ProfilerFormatter < XCPretty::Formatter
+class TravisProfilerFormatter < XCPretty::Formatter
 
   def initialize(use_unicode, colorize)
     super(use_unicode, colorize)
@@ -86,6 +86,51 @@ class ProfilerFormatter < XCPretty::Formatter
 
     return compile_times
   end
+
+  ###
+  ### Travis Formatting
+  ###
+  # from https://github.com/kattrali/xcpretty-travis-formatter
+def open_fold(text)
+    return if text == @open_fold
+    close_fold(@open_fold) if @open_fold
+    print "travis_fold:start:#{text}\r"
+    @open_fold = text
+  end
+
+  def close_fold(text)
+    print "travis_fold:end:#{text}\r"
+    @open_fold = nil
+  end
+
+  def format_build_target(target, project, configuration)
+    open_fold("Build")
+    super
+  end
+
+  def format_analyze_target(target, project, configuration)
+    open_fold("Analyze")
+    super
+  end
+
+  def format_clean_target(target, project, configuration)
+    open_fold("Clean")
+    super
+  end
+
+  def format_test_run_started(name)
+    open_fold("Tests-#{scrub(name)}")
+    super
+  end
+
+  def format_test_run_finished(name, time)
+    close_fold("Tests-#{scrub(name)}")
+    super
+  end
+
+  def scrub(text)
+    text.gsub(/\s/,"_").split(".").first
+  end
 end
 
-ProfilerFormatter
+TravisProfilerFormatter
